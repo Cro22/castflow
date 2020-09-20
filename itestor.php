90264c4de1bbb1c20d9ef606ccd110dcae5527bd
Jesus <jesus.nunez2050@gmail.com>
2020-09-20 06:29:13 -0400

    Updater Created

--- itestor.php
+++ itestor.php
@@ -9,3 +9,11 @@ Author: Jesus
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
+// Include our updater file
+include_once(plugin_dir_path(__FILE__) . '/updater/iTestor_Updater');
+
+$updater = new iTestor_Updater(__FILE__);
+$updater->set_username('cro22');
+$updater->set_repository('castflow');
+
+$updater->initialize();
